bca6732b5a1885440889d4988033045507afdd98
Andrii Vakarev <avakarev@gmail.com>
2015-11-17 11:57:02 +0100

    Use SSLv3 on report downloading

---
+++ lib/soapy_bing/helpers/ssl_version.rb
@@ -0,0 +1,17 @@
+require 'openssl'
+
+module SoapyBing
+  module Helpers
+    module SSLVersion
+      DEFAULT_SSL_VERSION = :SSLv3
+
+      def ssl_version
+        if OpenSSL::SSL::SSLContext::METHODS.include? DEFAULT_SSL_VERSION
+          DEFAULT_SSL_VERSION
+        else
+          OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version]
+        end
+      end
+    end
+  end
+end
---
+++ spec/soapy_bing/helpers/ssl_version_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe SoapyBing::Helpers::SSLVersion do
+  describe '#ssl_version' do
+    subject { stub_const('MyClass', Class.new.include(described_class)).new.ssl_version }
+
+    context 'when DEFAULT_SSL_VERSION is available' do
+      before do
+        stub_const('OpenSSL::SSL::SSLContext::METHODS', [described_class::DEFAULT_SSL_VERSION])
+      end
+
+      it 'responds with DEFAULT_SSL_VERSION' do
+        expect(subject).to be MyClass::DEFAULT_SSL_VERSION
+      end
+    end
+
+    context 'when DEFAULT_SSL_VERSION is not available' do
+      before { stub_const('OpenSSL::SSL::SSLContext::METHODS', []) }
+
+      it 'responds with OpenSSL default version' do
+        expect(subject).to be OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version]
+      end
+    end
+  end
+end
