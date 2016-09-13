628f05610502b4e51c5d2bf001659be74f7e4922
Guilherme Goettems Schneider <guigs81@gmail.com>
2016-09-13 11:09:07 -0300

    Rely on default ssl options

--- lib/soapy_bing/helpers/zip_downloader.rb
+++ lib/soapy_bing/helpers/zip_downloader.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
-require 'uri'
 require 'httparty'
 require 'zip'
 
 module SoapyBing
   module Helpers
     class ZipDownloader
-      DEFAULT_SSL_VERSION = :TLSv1
-
       def initialize(url)
         @url = url
       end
@@ -22,17 +19,7 @@ module SoapyBing
       private
 
       def download_io
-        https = URI.parse(@url).scheme == 'https'
-        request_options = https ? { ssl_version: ssl_version } : {}
-        StringIO.new HTTParty.get(@url, request_options).body
-      end
-
-      def ssl_version
-        if OpenSSL::SSL::SSLContext::METHODS.include? DEFAULT_SSL_VERSION
-          DEFAULT_SSL_VERSION
-        else
-          OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version]
-        end
+        StringIO.new HTTParty.get(@url).body
       end
     end
   end
--- spec/soapy_bing/helpers/zip_downloader_spec.rb
+++ spec/soapy_bing/helpers/zip_downloader_spec.rb
@@ -1,37 +1,16 @@
 # frozen_string_literal: true
 RSpec.describe SoapyBing::Helpers::ZipDownloader do
-  describe '#ssl_version' do
-    let(:empty_zip_file_path) { File.join('spec', 'fixtures', 'helpers', 'empty.zip') }
-    let(:empty_response) do
-      instance_double(HTTParty::Response, body: File.read(empty_zip_file_path))
+  describe '#read' do
+    let(:hello_zip_file_path) { File.join('spec', 'fixtures', 'helpers', 'hello.zip') }
+    let(:hello_response) do
+      instance_double(HTTParty::Response, body: File.read(hello_zip_file_path))
     end
     let(:download_url) { 'https://example.com/file.zip' }
-    subject(:instance) do
-      described_class.new(download_url)
-    end
+    subject(:instance) { described_class.new(download_url) }
 
-    context 'when DEFAULT_SSL_VERSION is available' do
-      before do
-        stub_const('OpenSSL::SSL::SSLContext::METHODS', [described_class::DEFAULT_SSL_VERSION])
-      end
-
-      it 'uses DEFAULT_SSL_VERSION' do
-        expect(HTTParty).to receive(:get).with(
-          download_url, ssl_version: described_class::DEFAULT_SSL_VERSION
-        ).and_return(empty_response)
-        instance.read
-      end
-    end
-
-    context 'when DEFAULT_SSL_VERSION is not available' do
-      before { stub_const('OpenSSL::SSL::SSLContext::METHODS', []) }
-
-      it 'uses OpenSSL default version' do
-        expect(HTTParty).to receive(:get).with(
-          download_url, ssl_version: OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version]
-        ).and_return(empty_response)
-        instance.read
-      end
+    it 'unzips and reads file content' do
+      expect(HTTParty).to receive(:get).with(download_url).and_return(hello_response)
+      expect(instance.read).to eq("Hello World\n")
     end
   end
 end
