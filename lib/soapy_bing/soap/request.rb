9f97bfb38ce258adee995a4fba222f80699fa0f5
Guilherme Goettems Schneider <guigs81@gmail.com>
2017-06-02 17:54:36 -0300

    Use savon client for Accounts API

--- lib/soapy_bing/soap/request.rb
+++ lib/soapy_bing/soap/request.rb
@@ -2,6 +2,5 @@
 require 'soapy_bing/soap/request/base'
 require 'soapy_bing/soap/request/submit_generate_report_request'
 require 'soapy_bing/soap/request/poll_generate_report_request'
-require 'soapy_bing/soap/request/get_accounts_info_request'
 require 'soapy_bing/soap/request/download_campaigns_by_account_ids_request'
 require 'soapy_bing/soap/request/get_bulk_download_status_request'
