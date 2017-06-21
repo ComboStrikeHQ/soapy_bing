4831957dbe864473bca3d482c7c37aab74470a5c
Guilherme Goettems Schneider <guigs81@gmail.com>
2017-06-21 16:23:06 -0300

    Add test for default polling settings

--- spec/soapy_bing/ads/campaign_performance_report_spec.rb
+++ spec/soapy_bing/ads/campaign_performance_report_spec.rb
@@ -5,29 +5,44 @@ require 'date'
 RSpec.describe SoapyBing::Ads::CampaignPerformanceReport do
   subject(:report) { described_class.new(report_options) }
 
-  let(:report_options) { { service_options: {} } }
+  let(:report_options) do
+    {
+      service_options: {},
+      date_start: date_start,
+      date_end: date_end
+    }
+  end
   let(:service_double) { double }
 
   describe '#initialize' do
-    let(:wrong_date) { 'wrong_date' }
-
     context 'when there is a wrong date format' do
-      before { report_options.merge!(date_start: wrong_date) }
-
-      let(:start_date) { 'wrong_date' }
+      let(:date_start) { 'wrong_date' }
+      let(:date_end) { 'today' }
 
       it 'throws exception' do
         expect { report }.to raise_error(ArgumentError, 'invalid date')
+      end
+    end
+
+    context 'with valid dates' do
+      let(:date_start) { '2017-06-20' }
+      let(:date_end) { '2017-06-21' }
+
+      it 'sets default pooling settings' do
+        polling_settings = report.polling_settings
+        expect(polling_settings[:tries]).to eq(20)
+        expect(polling_settings[:sleep].call(6)).to eq(64)
+        expect(polling_settings[:sleep].call(7)).to eq(120)
       end
     end
   end
 
   describe '#rows' do
     context 'with failed response' do
-      let(:date) { '2016-09-15' }
+      let(:date_start) { '2016-09-15' }
+      let(:date_end) { '2016-09-15' }
 
       before do
-        report_options.merge!(date_start: date, date_end: date)
         allow(SoapyBing::Service).to receive(:reporting).and_return(service_double)
         allow(service_double).to receive(:submit_generate_report) do
           { report_request_id: '123' }
